05ecdc3e838b4cb7caf3fcd0ae39bc85776fedfb
Thibaut-Padok <thibautr@padok.fr>
2023-03-09 20:55:42 +0100

    Initial Commit

---
+++ baywatch-app/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  baywatch-app
+//
+//  Created by thibaut robinet on 09/03/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

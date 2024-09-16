7f3e2e60e53c0dc459f5d90bb0467e97391215d6
Thibaut-Padok <thibautr@padok.fr>
2024-09-16 22:24:35 +0200

    feat(menu/client): add search bar

--- baywatch-app/menu/client/SearchMenuItem.swift
+++ baywatch-app/menu/client/SearchMenuItem.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import AppKit
+
+class SearchMenuItem: NSMenuItem {
+    weak var delegate: SearchItemViewEditingDelegate?
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    init(searchDelegate: SearchItemViewEditingDelegate, appDelegate: AppDelegate) {
+        super.init(title: "Search", action: #selector(appDelegate.noAction), keyEquivalent: "")
+        self.delegate = searchDelegate
+        let searchView = SearchView(frame: NSRect(x: 0, y: 0, width: 300, height: 30), searchDelegate: self.delegate! ) // Specify size here
+        self.view = searchView
+
+    }
+}
--- baywatch-app/menu/client/SearchView.swift
+++ baywatch-app/menu/client/SearchView.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+import AppKit
+import Cocoa
+
+class SearchView: NSView, NSSearchFieldDelegate {
+
+    weak var delegate: NSSearchFieldDelegate?
+    var searchDelegate: SearchItemViewEditingDelegate?
+
+    // Create the search field
+    private let searchField: NSSearchField = {
+        let field = NSSearchField()
+        field.placeholderString = "Search..."
+        field.translatesAutoresizingMaskIntoConstraints = false
+        return field
+    }()
+
+    init(frame frameRect: NSRect, searchDelegate: SearchItemViewEditingDelegate) {
+        super.init(frame: frameRect)
+        self.searchDelegate = searchDelegate
+        setupSearchField()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupSearchField()
+    }
+
+    private func setupSearchField() {
+        // Add the search field to the view
+        // Add the search field and set the delegate
+        searchField.delegate = self
+        addSubview(searchField)
+
+        // Set up Auto Layout constraints
+        NSLayoutConstraint.activate([
+            searchField.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            searchField.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0),
+            searchField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            searchField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5)
+        ])
+    }
+    func controlTextDidChange(_ obj: Notification) {
+        if let searchField = obj.object as? NSSearchField {
+            searchDelegate!.searchItemViewDidEdit(obj)
+        }
+    }
+}
